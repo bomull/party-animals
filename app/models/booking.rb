746fa1477180751bfd493807959034d6c555c9eb
gianmaria scioti <jimmyscioti@gmail.com>
2019-02-25 15:46:19 +0100

    validations for models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :party_animal
+  validates :user_id, :party_animal_id, presence: true
+  # TO DO VALIDATIONS FOR DATE IN PARTY_ANIMALS SCOPE
+  # WHEN WE FIGURE OUT HOW TO IMPLEMENT DATE
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_one :party_animal
+  validates :first_name, :email, :password, presence: true
+  validates :email, uniqueness: true
 end
