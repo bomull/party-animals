7d21a9b4fb4d7468dd612cf5972ab5b34a9d7196
gianmaria scioti <jimmyscioti@gmail.com>
2019-02-27 13:54:14 +0100

    completed user profile

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   end
 
   resources :bookings, only: [:destroy, :show]
+  resources :dashboard, only: :show
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
