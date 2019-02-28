21d32a3a444a18ffaa14401b13f92dafaa9bb578
Pessi Virta <haamutili@gmail.com>
2019-02-28 14:49:18 +0100

    added menu image to User seed. Fixed navbar to use image_tag. added flatpickr to PartyAnimal booking form.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,8 @@
 import "bootstrap";
+import flatpickr from "flatpickr";
+// import "flatpickr/dist/flatpickr.min.css";
+import "flatpickr/dist/themes/dark.css";
+
+flatpickr(".datepicker", {
+                            altInput: true,
+                         })
